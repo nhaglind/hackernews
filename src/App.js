cdad3a84fd66ecde3778114e97d5cf296398356a
Nick Haglind <nhaglind@gmail.com>
2017-01-11 09:37:46 -0600

    remove logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const list = [
   {
     title: 'React',
-    url: 'https://facebook.github.io/react',
+    url: 'https://facebook.github.io/react/',
     author: 'Jordan Walke',
     num_comments: 3,
     points: 4,
@@ -13,28 +12,26 @@ const list = [
   },
   {
     title: 'Redux',
-    url: 'https//github.com/reactjs/redux',
+    url: 'https://github.com/reactjs/redux',
     author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
     points: 5,
-    objectID: 1
+    objectID: 1,
   },
 ];
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
-        { list.map((item) => {
+        { list.map((item) =>
           <div key={item.objectID}>
             <span><a href={item.url}>{item.title}</a></span>
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
           </div>
-          );
-        })}
+        )}
       </div>
     );
   }
